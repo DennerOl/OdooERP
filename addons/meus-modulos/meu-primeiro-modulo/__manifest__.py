fec1985e3cc3491e3c53954d181dced337402bde
DennerOl <dener155@hotmail.com>
2024-10-11 20:24:03 -0300

    Projeto

---
+++ addons/meus-modulos/meu-primeiro-modulo/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    "name": "Meu primeiro módulo",
+    "description": "meu primeiro módulo",
+    "summary": "meu primeiro módulo que faz nada",
+    "author": "Fulano de ta",
+    
+}
---
+++ addons/meus-modulos/meu-primeiro-modulo/models/meu_primeiro_modelo.py
@@ -0,0 +1,7 @@
+from odoo import models
+
+"""comecei aqui criando meu primeiro modelo"""
+class MeuPrimeiroModelo(models.Model):
+
+    __name="meu.primeiro.modelo"
+    _description="descrição"
